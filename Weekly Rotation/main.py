a048d2552d1540c3b205b6c510118fc67bd6df1a
Mark O'Reilly <marko@megsupporttools.com>
2022-05-24 08:07:20 +0200

    added spy trend check to strategy

--- Weekly Rotation/main.py
+++ Weekly Rotation/main.py
@@ -1,20 +1,27 @@
 from QuantConnect import Resolution
 from QuantConnect.Algorithm import QCAlgorithm
-from QuantConnect.Indicators import RelativeStrengthIndex, RateOfChange
+from QuantConnect.Indicators import RelativeStrengthIndex, RateOfChange, SimpleMovingAverage
 
 
 class WeeklyRotation(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2002,1,1)
-        self.SetEndDate(2022,1,1)
+        self.SetStartDate(2002, 1, 1)
+        self.SetEndDate(2022, 1, 1)
         self.SetCash(10000)
         self.UniverseSettings.Resolution = Resolution.Daily
+        self.spy = self.AddEquity("SPY", Resolution.Daily)
         self.AddUniverse(self.coarse_selection, self.fine_selection)
         self.averages = {}
         self._changes = None
 
+    def update_spy(self):
+        history = self.History(self.spy.Symbol, 200, Resolution.Daily)
+        self.averages[self.spy.Symbol] = SPYSelectionData(history)
+        self.averages[self.spy.Symbol].update(self.Time, self.spy.Price)
+
     def coarse_selection(self, coarse):
+        self.update_spy()
         MIN_VOL = 1000000
         MIN_PRICE = 1
         return [stock.Symbol for stock in coarse if stock.Volume > MIN_VOL and stock.Price > MIN_PRICE]
@@ -42,7 +49,15 @@ class WeeklyRotation(QCAlgorithm):
         selected = sorted(selected, key=lambda stock: self.averages[stock.Symbol].roc, reverse=True)[0:10]
         return [stock.Symbol for stock in selected]
 
+    @property
+    def spy_downtrending(self) -> bool:
+        return self.averages[self.spy.Symbol].ma_200.Current.Value > self.spy.Price
+
     def OnData(self, data):
+        if self.spy_downtrending:
+            for security in self.Portfolio.Securities.keys():
+                self.Liquidate(self.Portfolio.Securities[security].Symbol)
+            return
         # if we have no changes, do nothing
         if self._changes is None: return
         # liquidate removed securities
@@ -50,7 +65,7 @@ class WeeklyRotation(QCAlgorithm):
             if security.Invested:
                 self.Liquidate(security.Symbol)
         for symbol in self.ActiveSecurities.Keys:
-            if not self.ActiveSecurities[symbol].Invested and \
+            if symbol != self.spy.Symbol and not self.ActiveSecurities[symbol].Invested and \
                     self.averages[symbol].is_ready() and self.averages[symbol].rsi.Current.Value < 50:
                 self.SetHoldings(symbol, 0.1)
         self._changes = None
@@ -74,3 +89,17 @@ class SelectionData():
     def update(self, time, price):
         self.rsi.Update(time, price)
         self.roc.Update(time, price)
+
+
+class SPYSelectionData():
+    def __init__(self, history):
+        self.ma_200 = SimpleMovingAverage(200)
+
+        for data in history.itertuples():
+            self.ma_200.Update(data.Index[1], data.close)
+
+    def is_ready(self):
+        return self.ma_200.IsReady
+
+    def update(self, time, price):
+        self.ma_200.Update(time, price)
