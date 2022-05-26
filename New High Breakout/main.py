ec1f92b88d0a0cd7d83a0bf41c1d650ff7cd53fc
Mark O'Reilly <marko@megsupporttools.com>
2022-05-26 17:20:43 +0200

    updated strategy

--- New High Breakout/main.py
+++ New High Breakout/main.py
@@ -1,11 +1,13 @@
 from QuantConnect import Resolution
 from QuantConnect.Algorithm import QCAlgorithm
-from QuantConnect.Indicators import RelativeStrengthIndex, RateOfChange, SimpleMovingAverage
+from QuantConnect.Brokerages import BrokerageName
+from QuantConnect.Indicators import SimpleMovingAverage, RateOfChange, Maximum
 
 
 class NewHighBreakout(QCAlgorithm):
 
     def Initialize(self):
+        self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage)
         self.SetStartDate(2021, 1, 1)
         self.SetEndDate(2022, 1, 1)
         self.SetCash(10000)
@@ -16,8 +18,9 @@ class NewHighBreakout(QCAlgorithm):
         self._changes = None
 
     def update_spy(self):
-        history = self.History(self.spy.Symbol, 200, Resolution.Daily)
-        self.averages[self.spy.Symbol] = SPYSelectionData(history)
+        if self.spy.Symbol not in self.averages:
+            history = self.History(self.spy.Symbol, 50, Resolution.Daily)
+            self.averages[self.spy.Symbol] = SPYSelectionData(history)
         self.averages[self.spy.Symbol].update(self.Time, self.spy.Price)
 
     def coarse_selection(self, coarse):
@@ -28,60 +31,66 @@ class NewHighBreakout(QCAlgorithm):
             if symbol == self.spy.Symbol:
                 continue
             if symbol not in self.averages:
-                history = self.History(symbol, 200, Resolution.Daily)
-                self.averages[symbol] = SelectionData(history)
-            self.averages[symbol].update(self.Time, stock.Price)
-            stocks.append(stock)
+                self.averages[symbol] = SelectionData(self.History(symbol, 50, Resolution.Daily))
+            self.averages[symbol].update(self.Time, stock)
+            if stock.Price > self.averages[symbol].ma.Current.Value:
+                stocks.append(stock)
         stocks = sorted(stocks, key=lambda stock: self.averages[stock.Symbol].roc, reverse=True)[:10]
         return [stock.Symbol for stock in stocks]
 
     @property
     def spy_downtrending(self) -> bool:
-        return self.averages[self.spy.Symbol].ma_200.Current.Value > self.spy.Price
+        return self.averages[self.spy.Symbol].ma.Current.Value > self.spy.Price
 
-    def OnData(self, data):
+    def OnData(self, slice) -> None:
         if self.spy_downtrending:
             for security in self.Portfolio.Securities.keys():
                 self.Liquidate(self.Portfolio.Securities[security].Symbol)
             return
-        # if we have no changes, do nothing
-        if self._changes is None: return
-        # liquidate removed securities
-        for security in self._changes.RemovedSecurities:
-            if security.Invested:
-                self.Liquidate(security.Symbol)
         for symbol in self.ActiveSecurities.Keys:
-            if symbol != self.spy.Symbol and not self.ActiveSecurities[symbol].Invested and \
-                    self.averages[symbol].is_ready() and self.averages[symbol].rsi.Current.Value < 50:
-                self.SetHoldings(symbol, 0.1)
-        self._changes = None
-
-    def OnSecuritiesChanged(self, changes):
-        self._changes = changes
+            if symbol == self.spy.Symbol:
+                continue
+            if self.ActiveSecurities[symbol].Invested:
+                if self.averages[symbol].is_ready() and \
+                        self.ActiveSecurities[symbol].Close < self.averages[symbol].ma.Current.Value:
+                    self.Liquidate(symbol)
+            else:
+                if self.averages[symbol].is_ready() and \
+                        self.ActiveSecurities[symbol].Close >= self.averages[symbol].highs.Current.Value:
+                    position_value = self.Portfolio.TotalPortfolioValue / 10
+                    if position_value < self.Portfolio.Cash:
+                        self.MarketOrder(symbol, int(position_value / self.ActiveSecurities[symbol].Price))
 
 
 class SelectionData():
     def __init__(self, history):
-        self.max = MAX(200)
+        self.roc = RateOfChange(50)
+        self.ma = SimpleMovingAverage(50)
+        self.highs = Maximum(50)
+
         for data in history.itertuples():
-            self.max.Update(data.Index[1], data.close)
+            self.roc.Update(data.Index[1], data.close)
+            self.ma.Update(data.Index[1], data.close)
+            self.highs.Update(data.Index[1], data.high)
 
     def is_ready(self):
-        return self.max.IsReady
+        return self.roc.IsReady and self.ma.IsReady
 
-    def update(self, time, price):
-        self.max.Update(time, price)
+    def update(self, time, stock):
+        self.roc.Update(time, stock.price)
+        self.ma.Update(time, stock.price)
+        self.highs.Update(time, stock.high)
 
 
 class SPYSelectionData():
     def __init__(self, history):
-        self.ma_200 = SimpleMovingAverage(200)
+        self.ma = SimpleMovingAverage(200)
 
         for data in history.itertuples():
-            self.ma_200.Update(data.Index[1], data.close)
+            self.ma.Update(data.Index[1], data.close)
 
     def is_ready(self):
-        return self.ma_200.IsReady
+        return self.ma.IsReady
 
     def update(self, time, price):
-        self.ma_200.Update(time, price)
+        self.ma.Update(time, price)
