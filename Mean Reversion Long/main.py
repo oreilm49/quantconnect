2f207260d88649b0b4794fd81519eaf1cd655c02
Mark O'Reilly <marko@megsupporttools.com>
2022-06-22 20:36:16 +0200

    moved ohlc into coarse

--- Mean Reversion Long/main.py
+++ Mean Reversion Long/main.py
@@ -12,33 +12,44 @@ class MeanReversionLong(QCAlgorithm):
     def Initialize(self):
         self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage)
         self.SetStartDate(2012, 1, 1)
-        self.SetEndDate(2022, 1, 1)
+        self.SetEndDate(2012, 2, 1)
         self.SetCash(10000)
         self.UniverseSettings.Resolution = Resolution.Daily
         self.AddUniverse(self.coarse_selection)
         self.coarse_averages = {}
+        self.atrs = {}
         self._changes = None
         self.EQUITY_RISK_PC = 0.01
 
     def coarse_selection(self, coarse):
         stocks = []
-        count = 0
-        coarse = [stock for stock in coarse if stock.DollarVolume > 2500000 and stock.Price > 1 and stock.Market == Market.USA and stock.HasFundamentalData]
-        for stock in sorted(coarse, key=lambda x: x.DollarVolume, reverse=True):
-            if count == 500:
-                break
+        # clear atrs each time so we don't accidentally use outdated data.
+        self.atrs = {}
+        coarse = sorted(
+            [stock for stock in coarse if stock.DollarVolume > 2500000 and stock.Price > 1 and stock.Market == Market.USA and stock.HasFundamentalData],
+            key=lambda x: x.DollarVolume, reverse=True
+        )[:500]
+        price_data = self.History([stock.Symbol for stock in coarse], 50, Resolution.Daily)
+        for stock in coarse:
             symbol = stock.Symbol
-            if symbol not in self.coarse_averages or self.coarse_averages[symbol].is_outdated(self.Time):
-                self.coarse_averages[symbol] = CoarseSelectionData(self.History(symbol, 50, Resolution.Daily))
-            else:
-                self.coarse_averages[symbol].update(self.Time, stock)
+            self.coarse_averages[symbol] = CoarseSelectionData(price_data.loc[symbol])
             # Rule #1: Stock must be in long term uptrend (above 50 day)
             # Rule #4: 3 day RSI is below 30
-            if self.coarse_averages[symbol].rsi.Current.Value < 30 and stock.Price > self.coarse_averages[symbol].ma.Current.Value:
-                stocks.append(stock)
-                count += 1
+            if not (self.coarse_averages[symbol].rsi.Current.Value < 30 and stock.Price > self.coarse_averages[symbol].ma.Current.Value):
+                continue
+            ohlc_data = OHLCData(price_data.loc[symbol], symbol)
+            # Rule #2: 7 day ADX above 45 (strong short term trend)
+            if not ohlc_data.adx.Current.Value > 45:
+                continue
+            natr = ohlc_data.atr.Current.Value / stock.Price
+            # Rule #3: ATR% above 4
+            if natr < 0.04:
+                continue
+            self.atrs[symbol] = ohlc_data.atr.Current.Value
+            stocks.append(stock)
         # Rule #6: Rank by the lowest RSI = most oversold stocks
-        return [stock.Symbol for stock in sorted(stocks, key=lambda x: self.coarse_averages[x.Symbol].rsi.Current.Value)]
+        symbols = [stock.Symbol for stock in sorted(stocks, key=lambda x: self.coarse_averages[x.Symbol].rsi.Current.Value)]
+        return symbols
 
     def position_outdated(self, symbol) -> bool:
         """
@@ -62,15 +73,9 @@ class MeanReversionLong(QCAlgorithm):
             else:
                 if self.ObjectStore.ContainsKey(NUM_OF_SYMBOLS) and int(self.ObjectStore.Read(NUM_OF_SYMBOLS)) >= 10:
                     continue
-                ohlc_data = OHLCData(self.History(symbol, 10, Resolution.Daily), symbol)
-                # Rule #2: 7 day ADX above 45 (strong short term trend)
-                if not ohlc_data.adx.Current.Value > 45:
+                if symbol not in self.atrs:
                     continue
-                natr = ohlc_data.atr.Current.Value / self.ActiveSecurities[symbol].Price
-                # Rule #3: ATR% above 4
-                if natr < 0.04:
-                    continue
-                position_size, position_value = self.calculate_position(symbol, ohlc_data.atr.Current.Value)
+                position_size, position_value = self.calculate_position(symbol, self.atrs[symbol])
                 if self.Portfolio.GetMarginRemaining(symbol, OrderDirection.Buy) > position_value:
                     self.MarketOrder(symbol, position_size)
                     self.ObjectStore.Save(str(symbol), str(self.Time))
@@ -90,15 +95,8 @@ class CoarseSelectionData():
         self.ma = SimpleMovingAverage(50)
 
         for data in history.itertuples():
-            self.rsi.Update(data.Index[1], data.close)
-            self.ma.Update(data.Index[1], data.close)
-
-    def update(self, time, stock):
-        self.rsi.Update(time, stock.Price)
-        self.ma.Update(time, stock.Price)
-
-    def is_outdated(self, time):
-        return (time - self.ma.Current.Time).days > 1 or (time - self.rsi.Current.Time).days > 1
+            self.rsi.Update(data.Index, data.close)
+            self.ma.Update(data.Index, data.close)
 
 
 class OHLCData():
@@ -111,6 +109,6 @@ class OHLCData():
             self.update(data)
 
     def update(self, data, time=None):
-        trade_bar = TradeBar(time or data.Index[1], self.symbol, data.open, data.high, data.low, data.close, data.volume, timedelta(1))
+        trade_bar = TradeBar(time or data.Index, self.symbol, data.open, data.high, data.low, data.close, data.volume, timedelta(1))
         self.adx.Update(trade_bar)
         self.atr.Update(trade_bar)
