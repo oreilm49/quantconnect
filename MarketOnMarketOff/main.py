d0ff1ca925355c65ca97c00aee1995b88131ce88
Mark O'Reilly <marko@megsupporttools.com>
2022-08-01 21:46:13 +0200

    added buy and sell logic based on ftd

--- MarketOnMarketOff/main.py
+++ MarketOnMarketOff/main.py
@@ -10,22 +10,37 @@ class MarketOnMarketOff(QCAlgorithm):
     def Initialize(self):
         self.SetStartDate(2021, 1, 24)
         self.SetCash(100000)
-        self.AddEquity("SPY", Resolution.Daily)
+        self.qqq = self.AddEquity("QQQ", Resolution.Daily)
+        self.sqqq = self.AddEquity("SQQQ", Resolution.Daily)
         self.data = None
 
     def OnData(self, slice: Slice):
         if not self.data:
-            self.data = SymbolData(self.History(self.Symbol('SPY'), 50, Resolution.Daily))
+            self.data = SymbolData(self.History(self.Symbol('QQQ'), 50, Resolution.Daily))
         else:
             self.data.update(self.Time, slice.close, slice.volume)
+            self.data.low_window.Add(slice.low)
+        signal = self.data.signal()
         if not self.Portfolio.Invested:
-            # If there are no recent distribution days and we're not invested, buy.
+            if signal == SIGNAL_BUY:
+                self.SetHoldings("QQQ", 1)
+            elif signal == SIGNAL_SELL:
+                self.SetHoldings("SQQQ", 1)
+        else:
+            if signal == SIGNAL_BUY and self.ActiveSecurities[self.sqqq].Invested:
+                self.Liquidate()
+                self.SetHoldings("QQQ", 1)
+            if signal == SIGNAL_SELL and self.ActiveSecurities[self.qqq].Invested:
+                self.Liquidate()
+                self.SetHoldings("SQQQ", 1)
 
 
 class SymbolData:
     def __init__(self, history):
         self.dd_window = RollingWindow[int](50)
         self.ftd_window = RollingWindow[int](50)
+        self.rally_day_window = RollingWindow[int](50)
+        self.low_window = RollingWindow[float](50)
         self.vol_ma = SimpleMovingAverage(50)
         self.max = Maximum(200)
         self.min = Minimum(200)
@@ -35,6 +50,7 @@ class SymbolData:
 
         for data in history.itertuples():
             self.update(data.Index[1], data.close, data.volume)
+            self.low_window.Add(data.low)
 
     def update(self, time, close, volume):
         self.vol_ma.Update(time, volume)
@@ -46,22 +62,38 @@ class SymbolData:
             is_follow_through_day = day_change > 0.17 and volume > self.previous_vol and volume > self.vol_ma.Current.Value
             self.dd_window.Add(1 if is_distribution_day else 0)
             self.ftd_window.Add(1 if is_follow_through_day else 0)
+            self.rally_day_window.Add(1 if day_change > 0 else 0)
         self.previous_close = close
         self.previous_vol = volume
 
-    def market_in_distribution(self):
-        return sum(list(self.dd_window)) > 5
-
-    def market_on(self):
+    def signal(self):
         """
         There has been a rally day and a follow through day after a recent bottom.
         The market isn't in distribution
         :return: boolean
         """
         if self.signal == SIGNAL_SELL:
-            self.min.PeriodsSinceMinimum
+            # find bottom, rally day, follow through day
+            # rally day = loop through each day after min day. Look for up day that starts the rally attempt
+            # follow through day = loop through each day after rally day. Check ftd conditions
+            rally_day_index = None
+            for day_index in reversed(range(self.min.PeriodsSinceMinimum)):
+                if self.rally_day_window[day_index] == 1:
+                    rally_day_index = day_index
+            if rally_day_index is None:
+                return False
+            for ftd_index in reversed(range(rally_day_index)):
+                if self.ftd_window[ftd_index] == 1:
+                    # check if ftd failed: if the low of the ftd was taken out.
+                    # if so, move on to the next ftd_index
+                    # if not, exit loop and update buy signal
+                    ftd_low_taken_out = np.min(self.low_window[0:ftd_index - 1]) < self.low_window[ftd_index]
+                    if ftd_low_taken_out:
+                        continue
+                    self.signal = SIGNAL_BUY
+                    break
         else:
-            return not self.market_in_distribution()
-
-    def market_off(self):
-        pass
+            market_in_distribution = sum(list(self.dd_window)) > 5
+            if market_in_distribution:
+                self.signal = SIGNAL_SELL
+        return self.signal
