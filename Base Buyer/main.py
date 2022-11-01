92bcc0321dab623bef46386546f74d99eba6a5d5
mark <markoreilly1992@gmail.com>
2022-11-01 12:20:29 +0700

    updated to use stocks from dropbox

--- Base Buyer/main.py
+++ Base Buyer/main.py
@@ -1,20 +1,57 @@
-import requests
+import base64
+
 
 class BaseBuyer(QCAlgorithm):
-
+    
     def Initialize(self):
-        self.SetStartDate(2020, 1, 1)
-        self.SetCash(10000)
-        tickers = requests.get("url").json()
-        for ticker in tickers:
-            self.AddEquity(ticker, Resolution.Day)
+        self.SetStartDate(2017, 7, 4)
+        self.SetEndDate(2018, 7, 4)
+        self.stocks_map = {}
+        self.open_positions = {}
+        self.stocks_file_link = 'https://www.dropbox.com/s/b9mxz0qyi9bxi4w/trades.csv?dl=0'
+        self.backtest_stocks_file_link = 'https://www.dropbox.com/s/t0atj97efii8zv6/backtest_trades.csv?dl=0'
+        self.EQUITY_RISK_PC = 0.75
+        self.UniverseSettings.Resolution = Resolution.Daily
+        # Order margin value has to have a minimum of 0.5% of Portfolio value, allows filtering out small trades and reduce fees.
+        self.Settings.MinimumOrderMarginPortfolioPercentage = 0.005
+        self.AddUniverse("my-dropbox-universe", self.universe_selector)
+
+    def universe_selector(self, date):
+        csv_str = self.Download(self.stocks_file_link if self.LiveMode else self.backtest_stocks_file_link)
+        for index, line in enumerate(csv_str.splitlines()):
+            if index == 0: continue
+            symbol, pivot, stop = line.split(',')
+            if symbol and pivot and stop:
+                self.stocks_map[symbol] = {
+                    'pivot': pivot,
+                    'stop': stop
+                }
+        return list(self.stocks_map.keys())
 
 
-    def OnData(self, data):
-        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
-            Arguments:
-                data: Slice object keyed by symbol containing the stock data
-        '''
+    def OnData(self, slice):
+        if slice.Bars.Count == 0: return
+        for symbol in self.ActiveSecurities.Keys:
+            if self.ActiveSecurities[symbol].Invested:
+                if self.Portfolio[symbol].UnrealizedProfitPercent >= 0.20 or self.position_outdated(symbol):
+                    self.Liquidate(symbol)
+            else:
+                pivot = self.stocks_map[str(symbol)]['pivot']
+                stock_in_buy_range = self.ActiveSecurities[symbol].Close > pivot < pivot * 1.05
+                if not stock_in_buy_range:
+                    continue
+                atr = AverageTrueRange(21)
+                vol_ma = RelativeDailyVolume(symbol, 50)
+                for data in self.History(symbol, 50, Resolution.Daily).itertuples():
+                    trade_bar = TradeBar(data.Index[1], data.Index[0], data.open, data.high, data.low, data.close, data.volume, timedelta(1))
+                    atr.Update(trade_bar)
+                    vol_ma.Update(trade_bar)
+                if vol_ma.Current.Value > 1:
+                    position_size = self.calculate_position_size(atr.Current.Value)
+                    position_value = position_size * self.ActiveSecurities[symbol].Price
+                    if position_value < self.Portfolio.Cash:
+                        self.MarketOrder(symbol, position_size)
+                        self.open_positions[symbol] = self.Time
 
-        # if not self.Portfolio.Invested:
-        #    self.SetHoldings("SPY", 1)
+    def calculate_position_size(self, atr):
+        return round((self.Portfolio.TotalPortfolioValue * self.EQUITY_RISK_PC) / atr)
