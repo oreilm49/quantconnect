66879be70e361b4208492dcc95b0335b1247ceb7
mark <markoreilly1992@gmail.com>
2023-01-12 13:15:32 +0000

    made equity risk pc configurable per strategy

--- MasterAlgo/base.py
+++ MasterAlgo/base.py
@@ -1,7 +1,10 @@
 class BaseStrategy:
+    def __init__(self, equity_risk_pc = 0.02) -> None:
+        self.equity_risk_pc = equity_risk_pc
+
     def calculate_position_size(self, algorithm, symbol):
         atr = algorithm.symbols[symbol].atr.Current.Value
-        return round((algorithm.Portfolio.TotalPortfolioValue * algorithm.EQUITY_RISK_PC) / atr)
+        return round((algorithm.Portfolio.TotalPortfolioValue * self.equity_risk_pc) / atr)
     
     def rebalance_due(self):
         raise NotImplementedError()
