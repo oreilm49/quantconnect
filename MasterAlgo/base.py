169c32f9a81f8a876a89d91a0d6ba216a70ee07c
mark <markoreilly1992@gmail.com>
2023-01-12 13:17:17 +0000

    updated default params of base

--- MasterAlgo/base.py
+++ MasterAlgo/base.py
@@ -1,3 +1,5 @@
+from AlgorithmImports import Resolution
+
 class BaseStrategy:
     def __init__(self, equity_risk_pc = 0.02) -> None:
         self.equity_risk_pc = equity_risk_pc
@@ -6,13 +8,13 @@ class BaseStrategy:
         atr = algorithm.symbols[symbol].atr.Current.Value
         return round((algorithm.Portfolio.TotalPortfolioValue * self.equity_risk_pc) / atr)
     
-    def rebalance_due(self):
-        raise NotImplementedError()
+    def rebalance_due(self, algorithm):
+        return algorithm.resolution == Resolution.Daily
     
     def handle_on_data(self, algorithm, data):
         raise NotImplementedError()
     
     def OnData(self, algorithm, data):
-        if not self.rebalance_due:
+        if not self.rebalance_due(algorithm):
             return
         self.handle_on_data(algorithm, data)
