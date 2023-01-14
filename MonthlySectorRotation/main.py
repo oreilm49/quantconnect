3982130bbac0221b0bb39885eb01350967b6f5f8
mark <markoreilly1992@gmail.com>
2023-01-14 16:36:16 +0000

    refactored algo

--- MonthlySectorRotation/main.py
+++ MonthlySectorRotation/main.py
@@ -1,108 +1,126 @@
-from collections import namedtuple
-from AlgorithmImports import *
-from typing import Dict, List, Tuple, Type
+import datetime
+from AlgorithmImports import RateOfChangePercent, SharpeRatio, AverageTrueRange, AlphaModel, Extensions, Resolution,\
+    Time, InsightDirection, InsightType, Insight, SimpleMovingAverage, RollingWindow, QCAlgorithm, BrokerageName, \
+    CompositeAlphaModel, ImmediateExecutionModel, NullRiskManagementModel, ConfidenceWeightedPortfolioConstructionModel
 
 
-class RocIndicators:
-    def __init__(self) -> None:
-        self.roc_10 = RateOfChangePercent(10)
-        self.roc_20 = RateOfChangePercent(20)
+class SymbolData:
+    def __init__(self, algorithm, security, resolution, roc_fast_lookback = None, roc_slow_lookback = None, atr_lookback = None, sharpe_lookback = None, sma_lookback = None):
+        self.security = security
+        self.symbol = security.Symbol
+        self.algorithm = algorithm
+        self.indicators = []
+        self.consolidator = algorithm.ResolveConsolidator(security.Symbol, resolution)
+        if atr_lookback:
+            self.atr = AverageTrueRange(atr_lookback)
+            self.indicators.append(self.atr)
+        if roc_fast_lookback:
+            self.roc_fast = RateOfChangePercent(roc_fast_lookback)
+            self.indicators.append(self.roc_fast)
+        if roc_slow_lookback:
+            self.roc_slow = RateOfChangePercent(roc_slow_lookback)
+            self.indicators.append(self.roc_slow)
+        if sharpe_lookback:
+            self.sharpe = SharpeRatio(sharpe_lookback)
+            self.indicators.append(self.sharpe)
+        if sma_lookback:
+            self.sma = SimpleMovingAverage(sma_lookback)
+            self.indicators.append(self.sma)
+        for indicator in self.indicators:
+            algorithm.RegisterIndicator(security.Symbol, indicator, self.consolidator)
+            algorithm.WarmUpIndicator(security.Symbol, indicator, resolution)
+
+    def dispose(self):
+        self.algorithm.SubscriptionManager.RemoveConsolidator(self.symbol, self.consolidator)
         
-    def update(self, trade_bar):
-        self.roc_10.Update(trade_bar.EndTime, trade_bar.Close)
-        self.roc_20.Update(trade_bar.EndTime, trade_bar.Close)
-    
-    @property
-    def ready(self):
-        return all((
-            self.roc_10.IsReady,
-            self.roc_20.IsReady,
-        ))
-    
     @property
     def monthly_performance(self) -> float:
-        return self.roc_10.Current.Value + (-2 * self.roc_20.Current.Value)
+        return self.roc_fast.Current.Value + (-2 * self.roc_slow.Current.Value)
+    
+    @property
+    def ready(self) -> bool:
+        return all([indicator.IsReady for indicator in self.indicators])
+    
+    def __str__(self) -> str:
+        values = ",".join([str(indicator) for indicator in self.indicators])
+        return f"{self.security.Symbol} ({values})"
     
 
-class MonthlyRotation(AlphaModel):
+class BaseAlpha(AlphaModel):
+    def __init__(self, *args, **kwargs):
+        self.equity_risk_pc = kwargs['equity_risk_pc']
+        self.resolution = kwargs['resolution']
+        self.month = None
+        self.indicators_map = {}
+        self.symbols = kwargs['symbols']
+        self.prediction_interval = Time.Multiply(Extensions.ToTimeSpan(self.resolution), 5) ## Arbitrary
+        self.indicator_kwargs = kwargs['indicator_kwargs']
+        self.spy = kwargs['spy']
+        self.spy_data = None
+
+    def get_confidence_for_symbol(self, algorithm, data, symbol):
+        algorithm.Debug(f"{algorithm.Time}: {self.indicators_map[symbol]}")
+        position_size = (algorithm.Portfolio.TotalPortfolioValue * self.equity_risk_pc) / self.indicators_map[symbol].atr.Current.Value
+        position_value = position_size * data[symbol].Close
+        return position_value / algorithm.Portfolio.TotalPortfolioValue
+    
+    def get_insight(self, algorithm, data, symbol, direction = InsightDirection.Up):
+        confidence = self.get_confidence_for_symbol(algorithm, data, symbol)
+        return Insight(symbol, self.prediction_interval, InsightType.Price, direction, confidence, None)
+    
+    def OnSecuritiesChanged(self, algorithm, changes):
+        for added in changes.AddedSecurities:
+            if added.Symbol == self.spy:
+                self.spy_data = SymbolData(
+                    algorithm, added, self.resolution, **dict(sma_lookback=150)
+                )
+            else:
+                self.indicators_map[added.Symbol] = SymbolData(
+                    algorithm, added, self.resolution, **self.indicator_kwargs
+                )
+
+        for removed in changes.RemovedSecurities:
+            data = self.indicators_map.pop(removed.Symbol, None)
+            if data is not None:
+                data.dispose()
+    
+
+class MonthlyRotation(BaseAlpha):
     """
     A U.S. sector rotation Momentum Strategy with a long lookback period
     """
     def __init__(self, *args, **kwargs):
-        self.symbols = kwargs['symbols']
-        self.look_back = kwargs['look_back']
+        super().__init__(*args, **kwargs)
         self.num = kwargs['num']
-        self.indicators_map = {}
-        self.month = None
-        self.prediction_interval = Time.Multiply(Extensions.ToTimeSpan(Resolution.Daily), 5) ## Arbitrary
-        self.spy = kwargs['spy']
-        self.spy_ma = SimpleMovingAverage(150)
-
-    def update_spy(self, data):
-        if not data.ContainsKey(self.spy) or data[self.spy] is None:
-            return
-        self.spy_ma.Update(data[self.spy].EndTime, data[self.spy].Close)
-
 
     def Update(self, algorithm, data):
-        symbols = []
-        for symbol in self.symbols:
-            if symbol not in self.indicators_map:
-                self.indicators_map[symbol] = SharpeRatio(self.look_back)
-            if not data.ContainsKey(symbol) or data[symbol] is None:
-                continue
-            self.indicators_map[symbol].Update(data[symbol].EndTime, data[symbol].Close)
-            if self.indicators_map[symbol].IsReady:
-                symbols.append(symbol)
-        if algorithm.Time.month == self.month:
+        if algorithm.Time.month == self.month or algorithm.IsWarmingUp:
             return []
         self.month = algorithm.Time.month
-        if not symbols:
-            return []
         highest_sharpe = sorted(
-            symbols, 
-            key=lambda symbol: self.indicators_map[symbol].Current.Value, 
+            self.symbols, 
+            key=lambda symbol: self.indicators_map[symbol].sharpe.Current.Value, 
             reverse=True,
         )[:self.num]
         # risk management
-        if data.ContainsKey(self.spy) and data[self.spy] is not None:
-            if data[self.spy].Close < self.spy_ma.Current.Value:
-                return [Insight(symbol, self.prediction_interval, InsightType.Price, InsightDirection.Flat, None, None) for symbol in algorithm.ActiveSecurities.Keys]
-        return [Insight(symbol, self.prediction_interval, InsightType.Price, InsightDirection.Up, None, None) for symbol in highest_sharpe]
+        if data.ContainsKey(self.spy) and data[self.spy] is not None and self.spy_data:
+            if data[self.spy].Close < self.spy_data.sma.Current.Value:
+                return [self.get_insight(algorithm, data, symbol, InsightDirection.Flat) for symbol in self.symbols]
+        return [self.get_insight(algorithm, data, symbol) for symbol in highest_sharpe]
             
 
-class BuyTheWorstMeanReversion(AlphaModel):
-    """
-    A U.S. sector rotation Mean Reversion Strategy
-    """
-    def __init__(self, *args, **kwargs):
-        self.symbols = kwargs['symbols']
-        self.indicators_map = {}
-        self.month = None
-        self.prediction_interval = Time.Multiply(Extensions.ToTimeSpan(Resolution.Daily), 5) ## Arbitrary
-
+class BuyTheWorstMeanReversion(BaseAlpha):
     def Update(self, algorithm, data):
-        symbols = []
-        for symbol in self.symbols:
-            if symbol not in self.indicators_map:
-                self.indicators_map[symbol] = RocIndicators()
-            if not data.ContainsKey(symbol) or data[symbol] is None:
-                continue
-            self.indicators_map[symbol].update(data[symbol])
-            if self.indicators_map[symbol].ready:
-                symbols.append(symbol)
-        if algorithm.Time.month == self.month:
+        if algorithm.Time.month == self.month or algorithm.IsWarmingUp:
             return []
         self.month = algorithm.Time.month
-        if not symbols:
-            return []
         worst_performer = sorted(
-            symbols, 
+            self.symbols,
             key=lambda symbol: self.indicators_map[symbol].monthly_performance, 
             reverse=True,
         )[0]
-        return [Insight(worst_performer, self.prediction_interval, InsightType.Price, InsightDirection.Up, None, None)]
-    
+        return [self.get_insight(algorithm, data, worst_performer)]    
+
 
 class Hedge(AlphaModel):
     """
@@ -164,11 +182,13 @@ class MonthlySectorRotation(QCAlgorithm):
         self.SetStartDate(2002, 1, 1)
         self.SetEndDate(2022, 11, 1)
         self.SetCash(10000)
-        self.SetWarmUp(timedelta(200), Resolution.Daily)
-        self.UniverseSettings.Resolution = Resolution.Daily
+        self.resolution = Resolution.Daily
+        self.SetWarmUp(datetime.timedelta(200), self.resolution)
+        self.UniverseSettings.Resolution = self.resolution
         self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage)
         self.Settings.RebalancePortfolioOnInsightChanges = False
         self.Settings.RebalancePortfolioOnSecurityChanges = False
+        self.equity_risk_pc = 0.02
         tickers = [
             "GXLE",
             "GXLK",
@@ -193,19 +213,27 @@ class MonthlySectorRotation(QCAlgorithm):
             "XLU",
         ]
         hedge_tickers = ["", ""] if self.LiveMode else ["SPY", "SH"]
-        symbols = [self.AddEquity(ticker, Resolution.Daily).Symbol for ticker in tickers]
-        spy = self.AddEquity(hedge_tickers[0], Resolution.Daily).Symbol
+        symbols = [self.AddEquity(ticker, self.resolution).Symbol for ticker in tickers]
+        spy = self.AddEquity(hedge_tickers[0], self.resolution).Symbol
+        alpha_kwargs = dict(
+            resolution=self.resolution,
+            equity_risk_pc=self.equity_risk_pc,
+            symbols=symbols,
+            spy=spy,
+        )
         self.SetAlpha(
             CompositeAlphaModel(
-                MonthlyRotation(symbols=symbols, look_back=198, num=1, spy=spy),
-                MonthlyRotation(symbols=symbols, look_back=7, num=1, spy=spy),
-                BuyTheWorstMeanReversion(symbols=symbols),
+                MonthlyRotation(**alpha_kwargs, num=1, indicator_kwargs=dict(sharpe_lookback=198, atr_lookback=21)),
+                MonthlyRotation(**alpha_kwargs, num=1, indicator_kwargs=dict(sharpe_lookback=7, atr_lookback=21)),
+                BuyTheWorstMeanReversion(**alpha_kwargs, indicator_kwargs=dict(
+                    roc_fast_lookback=10, roc_slow_lookback=20, atr_lookback=21
+                )),
                 Hedge(
                     spy=self.AddEquity(hedge_tickers[0], Resolution.Daily).Symbol,
                     hedge=self.AddEquity(hedge_tickers[1], Resolution.Daily).Symbol,
                 )
             )
         )
-        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel(self.DateRules.MonthStart()))
+        self.SetPortfolioConstruction(ConfidenceWeightedPortfolioConstructionModel(self.DateRules.MonthEnd()))
         self.SetExecution(ImmediateExecutionModel())
         self.SetRiskManagement(NullRiskManagementModel())
