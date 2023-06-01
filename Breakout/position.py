e56ea754fc7916e99335ff0fb1fd820535ae0a58
Mark O'Reilly <marko@megit.com>
2023-06-01 05:58:16 +0100

    removed stop price from position

--- Breakout/position.py
+++ Breakout/position.py
@@ -1,20 +1,16 @@
 import datetime
 import json
-from typing import Optional
 
 
 class Position:
     start: datetime.datetime
-    stop_price: Optional[float]
 
-    def __init__(self, start: datetime.datetime = None, stop_price: float = None) -> None:
+    def __init__(self, start: datetime.datetime = None) -> None:
         self.start = start
-        self.stop_price = stop_price
 
     def __str__(self) -> str:
         return json.dumps({
             "start": str(self.start),
-            "stop_price": self.stop_price
         })
 
 
