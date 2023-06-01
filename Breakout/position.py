f45cecc16d90743ee719c23f3ec5a6f2d997c52e
Mark O'Reilly <marko@megit.com>
2023-06-01 06:08:21 +0100

    added docsstring

--- Breakout/position.py
+++ Breakout/position.py
@@ -4,6 +4,10 @@ import json
 
 
 class Position:
+    """
+    Stores information relative to an open position.
+    Intended to be persisted in object storage as a json string.
+    """
     start: datetime.datetime
 
     def __init__(self, start: datetime.datetime = None) -> None:
@@ -16,6 +20,15 @@ class Position:
 
 
 def position_from_str(content: str) -> Position:
+    """
+    Method for parsing a Position from object storage.
+
+    Args:
+        content (str): the Position content as a json string.
+
+    Returns:
+        Position: The position class.
+    """
     content = json.loads(content)
     content['start'] = parse(content['start'])
     return Position(**content)
