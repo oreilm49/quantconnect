0c1367f5232c5c73bf495d18467278315ebed661
mark <markoreilly1992@gmail.com>
2023-09-20 07:09:52 +0100

    added atr

--- Reversal/indicators.py
+++ Reversal/indicators.py
@@ -1,4 +1,4 @@
-from AlgorithmImports import RollingWindow, TradeBar, Resolution
+from AlgorithmImports import RollingWindow, TradeBar, Resolution, AverageTrueRange
 from datetime import timedelta
 
 from reversal_day import ReversalDayIndicator
@@ -8,6 +8,7 @@ class SymbolIndicators:
     def __init__(self, algorithm, symbol) -> None:
         self.algorithm = algorithm
         self.trade_bar_window = RollingWindow[TradeBar](50)
+        self.atr = AverageTrueRange(21)
 
         history = algorithm.History(symbol, 200, Resolution.Daily)
         for data in history.itertuples():
@@ -16,7 +17,15 @@ class SymbolIndicators:
     
     def update(self, trade_bar):
         self.trade_bar_window.Add(trade_bar)
+        self.atr.Update(trade_bar)
     
     @property
     def reversal(self) -> ReversalDayIndicator:
         return ReversalDayIndicator(self.trade_bar_window)
+    
+    @property
+    def ready(self):
+        return all((
+            self.atr.IsReady,
+            self.trade_bar_window.IsReady,
+        ))
